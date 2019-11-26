51bfe2e24f67a8d5c39107e36320754c142292a8
Articho28 <artsiom.sk@gmail.com>
2019-11-26 18:22:36 -0500

    added rmvoe duplicates from sorted array

---
+++ solutions/src/Leetcode/Remove Duplicates From Sorted Array/solution.py
@@ -0,0 +1,19 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        if len(nums) < 1:
+            return 0
+        elif len(nums) == 1:
+            return 1
+        else:
+            current = nums[0]
+            index = 1
+            current_index = 1
+            while current_index < len(nums):
+                if nums[current_index] == current:
+                    current_index += 1
+                else:
+                    current = nums[current_index]
+                    nums[index] = nums[current_index]
+                    index += 1
+                    current_index += 1
+            return index
